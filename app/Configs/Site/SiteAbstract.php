9d5c5e2a8ec882b81fa5bc51fbdada22fdff2f35
“kieuvu” <kieuvuvuvu@gmail.com>
2022-08-31 17:05:38 +0700

    site abstract class

--- app/Configs/Site/SiteAbstract.php
+++ app/Configs/Site/SiteAbstract.php
@@ -7,19 +7,4 @@ abstract class SiteAbstract implements SiteInterface
     public function __construct()
     {
     }
-
-    public function isValidUrl(string $url): bool
-    {
-        return true;
-    }
-
-    public function canBeStored(string $url): bool
-    {
-        return true;
-    }
-
-    public function formatUrl(string $url): string
-    {
-        return $url;
-    }
 }
