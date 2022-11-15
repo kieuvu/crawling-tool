6be9638f9f6d1565eca90206ded5ecea7bac7345
“kieuvu” <kieuvuvuvu@gmail.com>
2022-11-15 17:20:47 +0700

    url helper

--- app/helper.php
+++ app/helper.php
@@ -12,3 +12,37 @@ function pdash()
 {
     BeautyEcho::Dash(intval(exec('tput cols')));
 }
+
+function extractUrlQueries($url,  $callback = null)
+{
+    [$urlString, $queryString] = array_pad(explode('?', $url), 2, '');
+    parse_str($queryString, $queriesArray);
+
+    try {
+        $newQueriesArray = is_callable($callback) ? $callback($queriesArray) : $queriesArray;
+    } catch (\Throwable) {
+        $newQueriesArray = $queriesArray;
+    }
+
+    $newQueryString = http_build_query($newQueriesArray);
+    if (strlen($newQueryString) == 0)
+        return $urlString;
+    return $urlString . '?' . $newQueryString;
+}
+
+function withoutUrlQueries($url, $blackLists = [])
+{
+    return extractUrlQueries($url, function (&$queriesArray) use ($blackLists) {
+        array_walk($blackLists, function ($v) use (&$queriesArray) {
+            unset($queriesArray[$v]);
+        });
+        return $queriesArray;
+    });
+}
+
+function onlyUrlQueries($url, $whiteLists = [])
+{
+    return extractUrlQueries($url, function (&$queriesArray) use ($whiteLists) {
+        return array_intersect_key($queriesArray, array_flip($whiteLists));
+    });
+}
