4dbe0a04b0f1fe07de7448a56619018fb7a5b4d9
“kieuvu” <kieuvuvuvu@gmail.com>
2022-11-22 09:32:32 +0700

    helper: replace url param

---
+++ app/helpers.php
@@ -0,0 +1,63 @@
+<?php
+
+use App\Libs\Console\BeautyEcho;
+
+if (!function_exists("pinfo")) {
+    function pinfo($label, $message = "")
+    {
+        logger()->info($label, [$message]);
+        BeautyEcho::Info($label, $message);
+    }
+}
+if (!function_exists("pdash")) {
+    function pdash()
+    {
+        BeautyEcho::Dash(intval(exec('tput cols')));
+    }
+}
+if (!function_exists("extractUrlQueries")) {
+    function extractUrlQueries($url,  $callback = null)
+    {
+        [$urlString, $queryString] = array_pad(explode('?', $url), 2, '');
+        parse_str($queryString, $queriesArray);
+
+        try {
+            $newQueriesArray = is_callable($callback) ? $callback($queriesArray) : $queriesArray;
+        } catch (\Throwable $ex) {
+            $newQueriesArray = $queriesArray;
+        }
+
+        $newQueryString = http_build_query($newQueriesArray);
+        if (strlen($newQueryString) == 0)
+            return $urlString;
+        return $urlString . '?' . $newQueryString;
+    }
+}
+if (!function_exists("withoutUrlQueries")) {
+    function withoutUrlQueries($url, $blackLists = [])
+    {
+        return extractUrlQueries($url, function (&$queriesArray) use ($blackLists) {
+            array_walk($blackLists, function ($v) use (&$queriesArray) {
+                unset($queriesArray[$v]);
+            });
+            return $queriesArray;
+        });
+    }
+}
+if (!function_exists("onlyUrlQueries")) {
+    function onlyUrlQueries($url, $whiteLists = [])
+    {
+        return extractUrlQueries($url, function (&$queriesArray) use ($whiteLists) {
+            return array_intersect_key($queriesArray, array_flip($whiteLists));
+        });
+    }
+}
+
+if (!function_exists("replaceParam")) {
+    function replaceParam($url, $newParam)
+    {
+        return extractUrlQueries($url, function (&$queriesArray) use ($newParam) {
+            return array_replace($queriesArray, $newParam);
+        });
+    }
+}
